b489e0d5b69bfef6bbd71c504bf0aca2fe1edd35
Pratyush <pratyushadhikary1152@gmail.com>
2020-08-05 19:23:08 +0530

    first commit

--- SongDew/settings.py
+++ SongDew/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import dj_database_url
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,7 +27,7 @@ SECRET_KEY = '0$3_**l+kr#36dvl+*ymwe^qjlxq796pfwu#(^()lp04*#$5v9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['pratyushassignment.herokuapp.com']
+ALLOWED_HOSTS = ['pratyushassignment.herokuapp.com', '*']
 
 
 # Application definition
@@ -45,7 +46,6 @@ INSTALLED_APPS = [
     'rest_auth.registration',
     'rest_framework.authtoken',
     'rest_auth',
-    'herokuapp',
     'Customer'
 ]
 
@@ -156,7 +156,9 @@ STATICFILES_DIRS = (
 )
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
+
+django_heroku.settings(locals())
